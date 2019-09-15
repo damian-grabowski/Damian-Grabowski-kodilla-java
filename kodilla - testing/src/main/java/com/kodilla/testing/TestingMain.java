ea9524f237df4ef9b56ff4441d382ed5236bfe43
damian-grb <damiangrabowski2910@gmail.com>
2019-09-15 14:22:02 +0200

    Zadanie nr 2

--- kodilla - testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla - testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,37 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main (String[] args) {
-        System.out.println("Moduł 6. Wprowdadzenie do testowania opragromowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+                if(result.equals("theForumUser")) {
+                    System.out.println("test OK");
+                } else {
+                    System.out.println("Error");
+                }
+
+        System.out.println("Test - pierwszy test jednostkowy ");
+
+        Calculator calculator = new Calculator(5,10);
+
+        double result1 = calculator.add();
+        if (result1 == 15) {
+            System.out.println("a = 5, b = 10");
+        } else {
+            System.out.println("Error");
+        }
+
+        double result2 = calculator.substract();
+        if (result2 == -5) {
+            System.out.println("a = 5, b = 10");
+        } else {
+            System.out.println("Error");
+        }
+
     }
 }
--- kodilla - testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
+++ kodilla - testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -1,4 +1,21 @@
 package com.kodilla.testing.calculator;
 
 public class Calculator {
+    double a, b;
+
+    public Calculator(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public double add() {
+        double result1 = a + b;
+        return result1;
+    }
+
+    public double substract() {
+        double result2 = a - b;
+        return result2;
+    }
 }
+
