c1250b2b40bfb84f58a71c765d95cee62d8ed67e
damian-grb <damiangrabowski2910@gmail.com>
2019-10-05 11:54:41 +0200

    Interfejs do pobierania danych

--- kodilla-testing/src/test/java/com/kodilla/testing/weather/mock/WeatherForecastTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/weather/mock/WeatherForecastTestSuite.java
@@ -1,4 +1,34 @@
 package com.kodilla.testing.weather.mock;
 
+import com.kodilla.testing.weather.stub.Temperatures;
+import com.kodilla.testing.weather.stub.WeatherForecast;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class WeatherForecastTestSuite {
-}
+    @Test
+    public void testCalcuateForecastWithMock() {
+        //Given
+        Temperatures temperaturesMock = mock(Temperatures.class);
+        HashMap<Integer, Double> temperaturesMap = new HashMap<Integer, Double>();
+        temperaturesMap.put(0, 25.5);
+        temperaturesMap.put(1, 26.6);
+        temperaturesMap.put(2, 27.8);
+        temperaturesMap.put(3, 32.3);
+        temperaturesMap.put(4, 28.6);
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+
+        //When
+        int quantityOfSensors = weatherForecast.calculateForecast().size();
+
+        //Then
+        Assert.assertEquals(5, quantityOfSensors);
+    }
+}   
