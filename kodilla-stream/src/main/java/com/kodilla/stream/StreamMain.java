c5ccb4e29743972a1d2b9b919f8b7199ea945614
damian-grb <damiangrabowski2910@gmail.com>
2019-11-26 16:42:35 +0100

    zmiana dostepnosci klasy

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,41 +1,12 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.iterate.NumbersGenerator;
-import com.kodilla.stream.lambda.Executor;
-import com.kodilla.stream.lambda.ExpressionExecutor;
-import com.kodilla.stream.lambda.Processor;
-import com.kodilla.stream.reference.FunctionalCalculator;
+import com.kodilla.stream.person.People;
+
 
 public class StreamMain {
     public static void main(String[] args) {
-//        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-//
-//        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-//        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-//        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-//        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
-//
-//        System.out.println("Calculating expressions with method references");
-//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
-
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        String beautifulText = poemBeautifier.beautify("Text to beautify", (text -> text.toUpperCase()));
-        System.out.println(beautifulText);
-
-        String beautifulText2 = poemBeautifier.beautify("Text to beautify", (text -> "ABC" + text + "ABC"));
-        System.out.println(beautifulText2);
-
-        String beautifulText3 = poemBeautifier.beautify("Text to beautify", (text -> text.replace(" ", "*")));
-        System.out.println(beautifulText3);
-
-        String beautifulText4 = poemBeautifier.beautify("Text to beautify", (text -> text.length() + " letters ***" + text + "*** " + text.length() + " letters"));
-        System.out.println(beautifulText4);
-
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
+        People.getList().stream()
+                .forEach(System.out::println);
     }
 }
+
