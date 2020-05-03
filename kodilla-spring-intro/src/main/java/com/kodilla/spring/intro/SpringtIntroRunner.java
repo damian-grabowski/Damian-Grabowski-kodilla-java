55dd9e8e638039830f8a157f5a38df4c5d361c58
damian-grb <damiangrabowski2910@gmail.com>
2020-05-03 13:34:39 +0200

    ForumUser

---
+++ kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/SpringtIntroRunner.java
@@ -0,0 +1,8 @@
+package com.kodilla.spring.intro;
+
+public class SpringtIntroRunner {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome into the intro spring");
+    }
+}
